3c362f3033acd694dc7f7effc8d6f93f9668aa57
Brometheus <zs.serrano1@gmail.com>
2022-08-13 00:55:36 -0500

    wrote out generatemarkdown function and functions on index.js

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
+const markdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+inquirer
+    .prompt([
+    {
+        name: 'title',
+        message: 'Project Title?',
+        type: 'input',
+    },
+    {
+        name: 'description',
+        message: 'Please describe your project',
+        type: 'input'
+    },
+    {
+        name: 'installation',
+        message: 'Please insert the installation process',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'Please describe the usage',
+        type: 'input'
+    },
+    {
+        name: 'contributors',
+        message: 'Please list all contributors',
+        type: 'input'
+    },
+    {
+        name: 'tests',
+        message: 'Please list all tests',
+        type: 'input'
+    }
+        ])
+    .then(function(answer) {
+    console.log(answer);
+    });
 
+const generator = generateMarkdown(questions);
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Congrats on your new README!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile('sample.md', generator)
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,8 +13,37 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+    \n
+    ## Table of Contents
+    \n 1. [Description](#description)
+    \n 2. [Installation](#installation)
+    \n 3. [Usage](#usage)
+    \n 4. [Contributors](#contributors)
+    \n 5. [Tests](#tests) 
+    ## Description <a href="description"></a>
+    \n 
+    ${data.description}
+    \n
+    ## Installation <a href="installation"></a>
+    \n 
+    ${data.installation}
+    \n
+    ## Usage <a href="usage"></a>
+    \n 
+    ${data.usage}
+    \n
+    ## Contributors <a href="contributors"></a>
+    \n 
+    ${data.contributors}
+    \n
+    ## Tests <a href="tests"></a>
+    \n 
+    ${data.tests}
+    \n
 `;
 }
 
 module.exports = generateMarkdown;
+module.exports = renderLicenseBadge;
+module.exports = renderLicenseLink;
+module.exports = renderLicenseSection;
